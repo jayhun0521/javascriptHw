ee8f4fd9762fe42f4aa561d31e691b9b965ad54a
jayhun0521 <jayhun21@gmail.com>
2022-10-09 15:58:16 -0400

    new push

---
+++ test.js
@@ -0,0 +1,22 @@
+//first alert
+alert("Hellow World");
+
+//second alert
+alert("hi again");
+
+
+console.log(20*5);
+
+document.write(20/5);
+
+var myVar = 10;
+
+myVar += 5;
+
+
+ilikemeat = true
+
+IlikeMeat = false
+7 > 5
+7 =7
+
